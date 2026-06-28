d9c57f2387a90f7933558b5bb848a5639492520f
CodeGunner100a <codegunner178@gmail.com>
2026-06-28 17:15:10 -0400

    Update RESPUESTA2.pas

--- RESPUESTA2.pas
+++ RESPUESTA2.pas
@@ -1,30 +1,75 @@
 //==========================================================================================
+//===== ESTO SOLO ES UN SIMULACRO PARA EL EXAMEN SERA OTRO REPOSITORIO EN OTRA CUENTA ======
+//==========================================================================================
+
 // Copiar el ejercicio 2 debajo del comentario "{ EJERCICIO 2 COPIAR ABAJO }"
+
+
 //EJERCICIO 2
-function InsertarDigitoEnPosicion(n: Integer; pos: Integer;
-  digito: Integer): Integer;
+function fct(n: Integer): Integer;
 var
-  numStr, parte1, parte2: string;
-  resultado: Integer;
+  i, f: Integer;
 begin
+  i := 0;
+  f := 1;
+  while i < n do
+  begin
+    i := i + 1;
+    f := f * i;
+  end;
+  Result := f;
+end;
 
-  numStr := IntToStr(n);
+function ejercicio2_series(n: Integer): real;
+var
+  x: Integer;
+  sw: Boolean;
+  cont: Integer;
+  numerador: Integer;
+  denomerador: Integer;
+  frac: real;
+  suma: real;
+begin
+  sw := True;
+  cont := 1;
+  suma := 0;
+  while n > c do
+  begin
+    c := c + 1;
 
-  parte1 := Copy(numStr, 1, pos);
-  parte2 := Copy(numStr, pos + 1, Length(numStr) - pos);
+    numerador := cont * cont; 
+    denomerador := fct(cont);
+    cont := cont + 1;
+    frac := numerador / denomerador;
 
-  numStr := parte1 + IntToStr(digito) + parte2;
+    if sw = True then
+    begin
+      
+      suma := suma - frac;
+      sw := False;
+    end
+    else
+    begin
+      
+      suma := suma + frac;
+      sw := True;
+    end;
 
-  resultado := StrToInt(numStr);
+  end;
 
-  InsertarDigitoEnPosicion := resultado;
+  Result := suma;
+
 end;
 
 
-
 //==========================================================================================
-// Copiar la llamada del ejercicio 2 debajo del comentario "{ EJERCICIO 2 COPIAR ABAJO }"
-//LLAMADA DEL EJERCICIO 2
+// Copiar la llamada del ejercicio 1 debajo del comentario "{ EJERCICIO 3 COPIAR ABAJO }"
 
-  Edit3.Text := IntToStr(InsertarDigitoEnPosicion(n, StrToInt(Edit2.Text),
-    StrToInt(Edit3.Text)));
+//LLAMADA DEL EJERCICIO 2
+ Edit3.Text := FloatToStr(ejercicio2_series(n));
+
+
+
+
+
+ 
