9a196845eccb98d63ffb93d78fef17eec67453b5
CodeGunner100a <codegunner178@gmail.com>
2026-06-27 23:06:05 -0400

    Update RESPUESTA1.pas

--- RESPUESTA1.pas
+++ RESPUESTA1.pas
@@ -1,5 +1,5 @@
 //==========================================================================================
-//CODEGUNNER
+//CODEGUNNER 010101010101
 // Copiar el ejercicio 1 debajo del comentario "{ EJERCICIO 1 COPIAR ABAJO }"
 //EJERCICIO 1
 function SumaProductos(n: Integer): real;
